55f9c5819f2eb107b4ab22c3253bb3da0d3f94be
Margaret E. Ikeda <margaretei@gmail.com>
2016-12-06 12:58:18 -0500

    starting to build function to get data, setting variables of get request

--- AC3.2-MidtermReview-Contacts/AC3.2-MidtermReview-Contacts/Contacts.swift
+++ AC3.2-MidtermReview-Contacts/AC3.2-MidtermReview-Contacts/Contacts.swift
@@ -17,4 +17,34 @@ class Contacts {
         self.lastName = lastName
         self.email = email
     }
+    func getData() {
+        let baseUrl = "https://api.fieldbook.com/v1/"
+        let bookId = "5846f3815fcbbc0300398b83"
+        let url = URL(baseUrl + bookId)
+    }
 }
+//    func newRequest() {
+//        let url = URL(string: "https://randomuser.me/api")!
+//        var request: URLRequest = URLRequest(url: url)
+//        request.httpMethod = "GET"
+//        //below will be important any time we do anything other than GET
+//        request.addValue("application/json", forHTTPHeaderField: "Accept")
+//
+//        let session = URLSession(configuration: URLSessionConfiguration.default)
+//        session.dataTask(with: request) { (data: Data?, _, _) in
+//            if data != nil {
+//                print("Hellz yeah DATA: \(data)")
+//
+//                do {
+//                    let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String : Any]
+//
+//                    if let validJson = json {
+//                        print(validJson)
+//                    }
+//                }
+//                catch {
+//                    print("Problems casting json: \(error)")
+//                }
+//            }
+//            }.resume()
+//}
