f5582710c3cb0e2a42878a832f4527bdfcb87737
Margaret E. Ikeda <margaretei@gmail.com>
2016-12-06 12:09:31 -0500

    first commit

---
+++ AC3.2-MidtermReview-Contacts/AC3.2-MidtermReview-Contacts/Contacts.swift
@@ -0,0 +1,9 @@
+//
+//  Contacts.swift
+//  AC3.2-MidtermReview-Contacts
+//
+//  Created by Margaret Ikeda on 12/6/16.
+//  Copyright © 2016 Margaret Ikeda. All rights reserved.
+//
+
+import Foundation
---
+++ AC3.2-MidtermReview-Contacts/AC3.2-MidtermReview-Contacts/ContactsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactsTableViewCell.swift
+//  AC3.2-MidtermReview-Contacts
+//
+//  Created by Margaret Ikeda on 12/6/16.
+//  Copyright © 2016 Margaret Ikeda. All rights reserved.
+//
+
+import UIKit
+
+class ContactsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
